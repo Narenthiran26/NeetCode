8970cbf3d612d858f26fcf28665568d8f8c3be99
Narenthiran26 <85405208+Narenthiran26@users.noreply.github.com>
2025-06-25 09:52:01 +0530

    Create Longest Substring Without Repeating Characters.java

---
+++ Longest Substring Without Repeating Characters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set=new HashSet<>();
+        int left=0;
+        int right=0;
+        int res=0;
+        while(right<s.length()){
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                res=Math.max(res,right-left+1);
+                right++;
+            }
+            else{
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return res;
+    }
+}
