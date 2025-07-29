7aeea0d1b1c8941e7d28d7ec3d94003a3ad4ac9b
Narenthiran26 <85405208+Narenthiran26@users.noreply.github.com>
2025-07-29 21:34:30 +0530

    Update Set Matrix Zeroes.java

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -1,24 +1,42 @@
-public class Solution {
+class Solution {
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length, cols = matrix[0].length;
-        boolean[] rowZero = new boolean[rows];
-        boolean[] colZero = new boolean[cols];
+        boolean firstRoZero=false;
+        boolean firstColZero=false;
+        int m=matrix.length;int n=matrix[0].length;
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0)firstColZero=true;
+        }
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0)firstRoZero=true;
+        }
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (matrix[r][c] == 0) {
-                    rowZero[r] = true;
-                    colZero[c] = true;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (rowZero[r] || colZero[c]) {
-                    matrix[r][c] = 0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
+
+        if(firstRoZero){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstColZero){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+
     }
 }
